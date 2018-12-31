e83d9c1577e10bae0f94c3251ec0ae3b47793ae2
Omnivoro <42907412+Omnivoro@users.noreply.github.com>
2018-12-31 15:44:55 +0000

    Creación del código Layout01.java

---
+++ Layouts/src/v081_layouts_01/Layout01.java
@@ -0,0 +1,50 @@
+package v081_layouts_01;
+
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Layout01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoLayout marco = new MarcoLayout();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marco.setVisible(true);
+	}
+
+}//Fin Clase Layout01
+
+class MarcoLayout extends JFrame{
+	
+	public MarcoLayout() {
+		
+		setBounds(800, 300, 300, 200);
+		setTitle("Marco Layout");
+		
+		PanelLayout lalamina = new PanelLayout();
+		
+		//v081-16:47 Creación del objeto FlowLayout
+		FlowLayout disposicion = new FlowLayout(FlowLayout.LEFT);
+		//v081-18:03 Utilazación del método setLayout() pertenciente a la clase "JPanel" que lo hereda de "Container" para darle el fotmato al objeto "lamina" pert
+		setLayout(disposicion);
+		
+		//v081-19:00 Código abreviado instaciadon el objeto FLowLayout dentro del método setLayout()
+		//setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		add(lalamina);
+		
+	}
+}
+
+class PanelLayout extends JPanel{
+	
+	 public PanelLayout(){
+		 add(new JButton("Amarillo"));
+		 add(new JButton("Rojo"));
+		 add(new JButton("Azul"));
+	 }
+}
+
+		 
